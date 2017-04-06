6944b30d66a54953c4fd937aa5f3ffcf79ab5fa8
simongrivc <simongrivc@gmail.com>
2017-04-06 02:21:10 +0200

    model
    model

--- app/Models/UporabnikModel.php
+++ app/Models/UporabnikModel.php
@@ -2,7 +2,7 @@
 namespace App;
 use Illuminate\Database\Eloquent\Model;
  
-class Uporabnika extends Model
+class Uporabnik extends Model
 { 
  	protected $fillable = ['IdUporabnik', 'ImeUporabnik'];	 
 }
