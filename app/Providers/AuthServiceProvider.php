2303f96a626218a1737aff5f28b650bbe2c05c7b
simongrivc <simongrivc@gmail.com>
2017-04-10 12:39:01 +0200

    authentification
    authentification

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,9 +31,15 @@ class AuthServiceProvider extends ServiceProvider
         // the User instance via an API token or any other method necessary.
 
         $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
+            $header = $request->header('Api-Token');
+            //if ($request->input('api_token')) {
+            //    return User::where('api_token', $request->input('api_token'))->first();
+            //}
+            if($header && $header = 'birds fly south')
+            {
+                return new User();
             }
+            return null;
         });
     }
 }
