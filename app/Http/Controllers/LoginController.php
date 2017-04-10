eb7a64c45b54e81871e4ff572c2126578543b71a
simongrivc <simongrivc@gmail.com>
2017-04-11 00:58:38 +0200

    Revert "jwt konfiguracija"
    This reverts commit 2352fdbf2338377a2693638e7c438ad81f37a2eb.
    
    # Conflicts:
    #	app/Http/Controllers/LoginController.php
    #	app/config/auth.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,16 +6,8 @@ use App\Uporabnik;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Tymon\JWTAuth\JWTAuth;
-
+ 
 class LoginController extends Controller{
-
-    protected $jwt;
-
-    public function __construct(JWTAuth $jwt)
-    {
-        $this->jwt = $jwt;
-    }
 
     public function ustvariUporabnika(Request $request){
  
@@ -42,40 +34,18 @@ class LoginController extends Controller{
     }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     public function login(Request $request){
 =======
     public function index(){
 >>>>>>> parent of ae8090e... login
+=======
+    public function login(){
+>>>>>>> parent of 2352fdb... jwt konfiguracija
  
         //$uporabniki = Uporabnik::all();
-       /* $uporabnik = Auth::user();
-        return response()->json($uporabnik);*/
-
-         $this->validate($request, [
-            'ImeUporabnik'    => 'required|max:255',
-        ]);
-
-        try {
-
-            if (! $token = $this->jwt->attempt($request->only('ImeUporabnik'))) {
-                return response()->json(['user_not_found'], 404);
-            }
-
-        } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
-
-            return response()->json(['token_expired'], 500);
-
-        } catch (\Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-
-            return response()->json(['token_invalid'], 500);
-
-        } catch (\Tymon\JWTAuth\Exceptions\JWTException $e) {
-
-            return response()->json(['token_absent' => $e->getMessage()], 500);
-
-        }
-
-        return response()->json(compact('token'));
+        $uporabnik = Auth::user();
+        return response()->json($uporabnik);
  
     }
 }
--- app/Models/UporabnikModel.php
+++ app/Models/UporabnikModel.php
@@ -1,33 +1,12 @@
 <?php
 namespace App;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Auth\Authenticatable;
-use Laravel\Lumen\Auth\Authorizable;
-//use Illuminate\Database\Eloquent\Model;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Tymon\JWTAuth\Contracts\JWTSubject;
  
-class Uporabnik extends Model implements JWTSubject, AuthenticatableContract, AuthorizableContract
+class Uporabnik extends Model
 { 
-	use Authenticatable, Authorizable;
  	protected $fillable = ['IdUporabnik', 'ImeUporabnik'];
 
  	protected $table = 'uporabniki';
- 	protected $primaryKey = 'IdUporabnik';
-
- 	/*protected $hidden = [
-        'password',
-    ];*/
-
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
+ 	  protected $primaryKey = 'IdUporabnik';
 }
 ?>
