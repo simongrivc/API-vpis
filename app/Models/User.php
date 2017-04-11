c6afdd937645b78b9360b7989717daaa52940cb3
simongrivc <simongrivc@gmail.com>
2017-04-11 23:44:44 +0200

    test
    test

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Eloquent\Model;
  
 class User extends Model
 { 
- 	protected $fillable = ['activation_code', 'username', 'email', 'is_active', 'name', 'surname'];
+ 	protected $fillable = ['api_token', 'activation_code', 'username', 'email', 'is_active', 'name', 'surname'];
 
  	protected $hidden = [
         'password',
