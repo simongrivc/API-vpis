e54477ce6c10ab5ce1915e10d81b678d38f08362
simongrivc <simongrivc@gmail.com>
2017-04-11 23:56:51 +0200

    prijava
    prijava

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,8 @@ class User extends Model
 { 
  	protected $fillable = ['api_token', 'activation_code', 'username', 'email', 'is_active', 'name', 'surname'];
 
+ 	protected $attributes  = ['api_token', 'activation_code', 'username', 'email', 'is_active', 'name', 'surname'];
+ 
  	protected $hidden = [
         'password',
     ];
