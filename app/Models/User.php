70f660869f1f61b7379e4944e4c25bf2b2e6219c
simongrivc <simongrivc@gmail.com>
2017-04-11 22:22:31 +0200

    user
    user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Eloquent\Model;
  
 class User extends Model
 { 
-	use Authenticatable, Authorizable;
  	protected $fillable = ['activation_code', 'username', 'email', 'is_active', 'name', 'surname'];
 
  	protected $hidden = [
