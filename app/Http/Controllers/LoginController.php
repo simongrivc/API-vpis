87438cf5a90ae29bf69a565e047eaa6d5aabde6d
simongrivc <simongrivc@gmail.com>
2017-04-11 23:31:35 +0200

    test
    test

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -16,8 +16,8 @@ class LoginController extends Controller{
         $p = $request->input('password');
 
         $user = User::where([
-                    ['username', '=', $u],
-                    ['password', '=', $p],
+                    ['username', '=', '$u'],
+                    ['password', '=', '$p'],
                 ])->first();
 
         //$user = User::whereName($username)->wherePassword($password)->first();
