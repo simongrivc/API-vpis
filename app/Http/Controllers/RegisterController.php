bbaddd537675e2c8bfe3c0c2eb27ba1bc0b3012e
simongrivc <simongrivc@gmail.com>
2017-04-12 13:04:42 +0200

    registracija
    registracija

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -13,19 +13,7 @@ class RegisterController extends Controller{
   
     public function registerStudent(Request $request){
         
-        //dobi podatke o študentu
-        //preveri pravilno strukturo emaila preveri podvajanje up imena in email naslova in šibkost gesla
-        //vrni errorje če je kaj narobe drugače dodaj userja z šifriranim geslom
-        //če ne dodaj v tabelo ter pošlji email z potrditvenim url jem v bazo dodaj potditev
-        //naredi endpoint za potrditve
-
-       
-       
-    }
-
-    public function registerUser(Request $request){
-        
-        //dobi podatke o študentu
+             //dobi podatke o študentu
         //preveri pravilno strukturo emaila preveri podvajanje up imena in email naslova in šibkost gesla
         //vrni errorje če je kaj narobe drugače dodaj userja z šifriranim geslom
         //če ne dodaj v tabelo ter pošlji email z potrditvenim url jem v bazo dodaj potditev
@@ -79,12 +67,15 @@ class RegisterController extends Controller{
             //$user = User::create($request->all());
      
             return response()->json($user);
+        }   
+         return  response()->json("Missing form data.");
+       
+       
+    }
 
-        }
+    public function registerUser(Request $request){
         
-        return  response()->json("Missing form data.");
-       
-       
+ 
     }
     
 
