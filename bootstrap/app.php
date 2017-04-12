855acf70e8fe78549605a6fc1726f9de075def5b
simongrivc <simongrivc@gmail.com>
2017-04-12 16:19:07 +0200

    mail
    mail

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -77,11 +77,11 @@ $app->singleton(
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
  $app->register(App\Providers\AuthServiceProvider::class);
 
  
-// $app->register(App\Providers\EventServiceProvider::class);
+$app->register(Illuminate\Mail\MailServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
