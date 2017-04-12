aec159faf378a5d11d84dbb6637a8b36090d2e2b
simongrivc <simongrivc@gmail.com>
2017-04-13 01:16:28 +0200

    hash
    hash

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller{
 
