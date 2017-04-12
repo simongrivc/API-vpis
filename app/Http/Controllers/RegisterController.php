2aa5c366c2803ef10a92ee2e39e34023c449fd66
simongrivc <simongrivc@gmail.com>
2017-04-12 16:30:25 +0200

    mail
    mail

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Mail;
 
 class RegisterController extends Controller{
 
@@ -77,7 +76,7 @@ class RegisterController extends Controller{
                     ['name' => $request->input('name'), 'surname' => $request->input('surname'), 'username' => $request->input('username'), 'password' => $request->input('password'), 'email' => $request->input('email'), 'fk_user_role' => 4, 'is_active' => 0, 'fk_activation_code' => $idActivationCodeUser]
                 );*/
                 //pošlji mail
-                Mail::raw('To je mail', function($msg) { $msg->to([ $request->input('email')]); $msg->from(['sistem@test.com']); });
+                \Mail::raw('To je mail', function($msg) { $msg->to([ $request->input('email')]); $msg->from(['sistem@test.com']); });
         
                 return response()->json('Student created (mail sent to student): ' . $activationCode);
             }
