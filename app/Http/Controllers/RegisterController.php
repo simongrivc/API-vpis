65034e47faf21d6bbf0bcfd4ef03f241db256326
simongrivc <simongrivc@gmail.com>
2017-04-12 19:24:08 +0200

    registracija admin
    registracija admin

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -16,14 +16,6 @@ class RegisterController extends Controller{
   
 
     public function registerStudent(Request $request){
-        
-             //dobi podatke o študentu
-        //preveri pravilno strukturo emaila preveri podvajanje up imena in email naslova in šibkost gesla
-        //vrni errorje če je kaj narobe drugače dodaj userja z šifriranim geslom
-        //če ne dodaj v tabelo ter pošlji email z potrditvenim url jem v bazo dodaj potditev
-        //naredi endpoint za potrditve
-
-        //preverimo podvajanje email naslova in username-a
 
         if($request->input('name') && $request->input('surname') && $request->input('username') && $request->input('password') && $request->input('email'))
         {
@@ -77,10 +69,7 @@ class RegisterController extends Controller{
             }
           
 
-            //dodaj v tabelo user activations nov zapis za študenta ter pošli mail :TODO
-
-
-            //$user = User::create($request->all());
+            //dodaj v tabelo user activations nov zapis za študenta ter pošli mail :TODO 
 
         }   
          return  response()->json("Missing form data.");
@@ -89,8 +78,69 @@ class RegisterController extends Controller{
     }
 
     public function registerUser(Request $request){
-        
+        $user = Auth::user();
+        //preveri da registrira administrator
+        if($user->fk_user_role==1)
+        {
+            if($request->input('name') && $request->input('surname') && $request->input('username') && $request->input('password') && $request->input('email') && $request->input('user_role'))
+            {
+                 // ujemanje username-a ali email naslova
+                $usernamesMatch = User::where('username', '=', $request->input('username'))->get();
+               
+                $emailMatch =  User::where('email', '=', $request->input('email'))->get();
+               
 
+                if(sizeOf($usernamesMatch)>0 || sizeOf($emailMatch)>0){
+                    //uporabnik že obstaja oz. se njegovi podatki podvajajo
+                    return response()->json('User duplication.');
+                }
+
+                //preverjanje ustreznosti gesla
+
+                if (strlen($request->input('password')) < 8) {
+                    return response()->json('Password too short (at least 8 characters)!');
+                }
+
+                if (!preg_match("#[0-9]+#", $request->input('password'))) {
+                    return response()->json('Password must include at least one number!');
+                }
+
+                if (!preg_match("#[a-zA-Z]+#", $request->input('password'))) {
+                    return response()->json('Password must include at least one letter!');
+                }     
+
+                //če gre vse čez kreiraj uporabnika ter ga dodaj v tabelo
+
+                //kreiram aktivacijsko kodo za link
+                $activationCode = Crypt::encrypt('Time created:;'.time().';Time of exp.:;'. (time()+3600) .';'. $request->input('username'));
+                
+                $idActivationCodeUser = DB::table('user_activations')->insertGetId(
+                    ['activation_code' => $activationCode, 'send_time' => time()]
+                );
+
+                if($idActivationCodeUser)
+                {
+                    $id = DB::table('users')->insertGetId(
+                        ['name' => $request->input('name'), 'surname' => $request->input('surname'), 'username' => $request->input('username'), 'password' => $request->input('password'), 'email' => $request->input('email'), 'fk_user_role' => $request->input('user_role'), 'is_active' => 1, 'fk_activation_code' => $idActivationCodeUser]
+                    );
+                    
+                    //popravi pri kreiranju is_active na 0 pošlji mail z aktivacijskim linkom :TODO
+                   
+                    return response()->json('User created.' . $activationCode);
+                }
+                else
+                {
+                    return response()->json("Activation code not generated error.");
+                }
+              
+
+                //dodaj v tabelo user activations nov zapis za študenta ter pošli mail :TODO 
+
+            }   
+             return  response()->json("Missing form data.");
+        }
+        
+        return  response()->json("This user doesn't have admin rights.");
     }
     
 
