8016c7220cb73e0f09b7988f33f881dd580072d4
simongrivc <simongrivc@gmail.com>
2017-04-12 12:53:35 +0200

    typo
    typo

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -65,7 +65,7 @@ class RegisterController extends Controller{
             $user->name = $request->input('name');
             $user->surname = $request->input('surname');
             $user->username = $request->input('email');
-            $user->password = Hash::make($request->input('password');
+            $user->password = Hash::make($request->input('password'));
             $user->email = $request->input('email');
             $user->save();
 
