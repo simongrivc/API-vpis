302a3539fe7834f87a7f9ddc0397fa7119a87126
simongrivc <simongrivc@gmail.com>
2017-04-12 18:05:27 +0200

    test
    test

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
-//use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Mail;
 
 class RegisterController extends Controller{
 
