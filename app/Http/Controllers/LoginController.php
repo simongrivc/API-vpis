cff1582a78f35f51367936e2152c857cd420c91f
simongrivc <simongrivc@gmail.com>
2017-04-12 22:43:21 +0200

    ip
    ip

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller{
 
