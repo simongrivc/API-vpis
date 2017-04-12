64af0d61e70bc5c273a060168458c84ed9b3b93f
simongrivc <simongrivc@gmail.com>
2017-04-12 18:02:43 +0200

    test
    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-           $this->app->singleton('mailer', function ($app) {
-            $app->configure('services');
-            return $app->loadComponent('mail', 'Illuminate\Mail\MailServiceProvider', 'mailer');
-        });
+      
     }
 }
