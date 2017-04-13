278ff70eacc14091e448c6d1ea5a1fda07a63236
simongrivc <simongrivc@gmail.com>
2017-04-13 03:31:57 +0200

    test
    test

--- app/Http/Middleware/CORSMiddleware.php
+++ app/Http/Middleware/CORSMiddleware.php
@@ -16,8 +16,8 @@ class CorsMiddleware
         $headers = [
             'Access-Control-Allow-Origin'      => '*',
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age'           => '86400',
+            'Access-Control-Allow-Credentials' => '*',
+            'Access-Control-Max-Age'           => '0',
             'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With, Api-Token'
         ];
 
