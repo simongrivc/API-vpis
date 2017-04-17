7184ca4c20e9a130f63a928895ddc6787856f716
simongrivc <simongrivc@gmail.com>
2017-04-18 00:48:26 +0200

    studyPrograms update (view)
    studyPrograms update (view)

--- app/Models/StudyPrograms.php
+++ app/Models/StudyPrograms.php
@@ -5,12 +5,11 @@ use Illuminate\Database\Eloquent\Model;
 class StudyProgram extends Model
 { 
 	//id fk_id_type fk_id_program_carrier	sequence_number	serial_id	program_name
- 	protected $fillable = ['fk_id_type', 'fk_id_program_carrier', 'sequence_number', 'serial_id', 'program_name'];
+ 	protected $fillable = ['id', 'fk_id_type', 'fk_id_program_carrier', 'sequence_number', 'serial_id', 'program_name', 'type_name', 'institution_name', 'abbreviation', 'fk_id_municipality', 'fk_id_university', 'municipality_name', 'university_name'];
 
- 	protected $attributes  = ['fk_id_type', 'fk_id_program_carrier', 'sequence_number', 'serial_id', 'program_name'];
+ 	protected $attributes  = ['id', 'fk_id_type', 'fk_id_program_carrier', 'sequence_number', 'serial_id', 'program_name', 'type_name', 'institution_name', 'abbreviation', 'fk_id_municipality', 'fk_id_university', 'municipality_name', 'university_name'];
  
-
- 	protected $table = 'study_programs';
+ 	protected $table = 'study_programs_view';
  	//da eloquent ne dodaja timestamp v bazo
  	public $timestamps  = false;
 }
