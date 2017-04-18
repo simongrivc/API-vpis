e59ded6fff2297c41c978b8faef8b4a81ec79d60
TursicKlemen <tursic.klemen@gmail.com>
2017-04-18 02:12:08 +0200

    Activation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
  
 class UserController extends Controller{
 
