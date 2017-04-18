bb6de246a48bfe967fc151714b1bb146d7159f7a
TursicKlemen <tursic.klemen@gmail.com>
2017-04-18 02:46:29 +0200

    Register is_active false

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -61,7 +61,7 @@ class RegisterController extends Controller{
             if($idActivationCodeUser)
             {
                 $id = DB::table('users')->insertGetId(
-                    ['name' => $request->input('name'), 'surname' => $request->input('surname'), 'username' => $request->input('username'), 'password' =>  Hash::make($request->input('password')), 'email' => $request->input('email'), 'fk_user_role' => 4, 'is_active' => 1, 'fk_activation_code' => $idActivationCodeUser]
+                    ['name' => $request->input('name'), 'surname' => $request->input('surname'), 'username' => $request->input('username'), 'password' =>  Hash::make($request->input('password')), 'email' => $request->input('email'), 'fk_user_role' => 4, 'is_active' => 0, 'fk_activation_code' => $idActivationCodeUser]
                 );
                 
                 //popravi pri kreiranju is_active na 0 pošlji mail z aktivacijskim linkom :TODO
