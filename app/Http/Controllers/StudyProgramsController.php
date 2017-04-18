d343bd36a86530a3bfd1f05a0e15b17054475a08
TursicKlemen <tursic.klemen@gmail.com>
2017-04-18 10:20:10 +0200

    Faculties

--- app/Http/Controllers/StudyProgramsController.php
+++ app/Http/Controllers/StudyProgramsController.php
@@ -6,6 +6,7 @@ use App\StudyProgram;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
  
 class StudyProgramsController extends Controller{
 
