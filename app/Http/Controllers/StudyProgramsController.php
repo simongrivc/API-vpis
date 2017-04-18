be03b1ed2823d7d2565df723c4d6953b97bd9690
simongrivc <simongrivc@gmail.com>
2017-04-18 21:34:57 +0200

    typo
    typo

--- app/Http/Controllers/StudyProgramsController.php
+++ app/Http/Controllers/StudyProgramsController.php
@@ -25,7 +25,7 @@ class StudyProgramsController extends Controller{
 
     public function getStudyProgramCalls(Request $request){
 
-        $studyProgramCalls = DB::select("SELECT * FROM study_program_view");
+        $studyProgramCalls = DB::select("SELECT * FROM study_programs_view");
 
         return response()->json($studyProgramCalls);
     }
