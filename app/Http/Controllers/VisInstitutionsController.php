fb376f5edc26f95c8730c43176660592bdc8d40b
TursicKlemen <tursic.klemen@gmail.com>
2017-04-25 19:48:09 +0200

    VisInstitutions update

--- app/Http/Controllers/VisInstitutionsController.php
+++ app/Http/Controllers/VisInstitutionsController.php
@@ -11,12 +11,18 @@ use Illuminate\Support\Facades\DB;
 class VisInstitutionsController extends Controller{
 
     public function index(){
+        $university = "";
+        
+        if($request->input('id_university')){
+            $university = " WHERE u.iduniversity = ".$request->input('id_university');
+        }
+        
         $institutions = DB::select("SELECT inst.id, inst.institution_name AS name, inst.abbreviation,
                                    m.id AS id_municipality, m.municipality_name, u.id AS id_university,
                                    u.university_name
                                    FROM vis_institutions inst
                                    INNER JOIN municipalities m ON inst.fk_id_municipality = m.id
-                                   INNER JOIN universities u ON inst.fk_id_university = u.id");
+                                   INNER JOIN universities u ON inst.fk_id_university = u.id".$university);
         
         return response()->json($institutions);
     }
--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,7 @@ $app->get('application/study_programs/{id}', 'StudyProgramsController@getStudyPr
 $app->get('application/study_programs_calls', 'StudyProgramsController@getStudyProgramCalls');
 //dobi vse fakultete
 $app->get('application/vis_institutions', 'VisInstitutionsController@index');
+$app->post('application/vis_institutions', 'VisInstitutionsController@index');
 
 
 
