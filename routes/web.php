c1d9091218e633bbe4c471b64616f6d4cd5d6a4f
TursicKlemen <tursic.klemen@gmail.com>
2017-04-25 20:18:37 +0200

    VisInstitutions

--- routes/web.php
+++ routes/web.php
@@ -49,8 +49,7 @@ $app->get('application/study_programs/{id}', 'StudyProgramsController@getStudyPr
 //dobi vse razpisane programe
 $app->get('application/study_programs_calls', 'StudyProgramsController@getStudyProgramCalls');
 //dobi vse fakultete
-//$app->get('application/vis_institutions', 'VisInstitutionsController@index');
-$app->post('application/vis_institutionsUniversity', 'VisInstitutionsController@getVisByUniversity');
+$app->post('application/vis_institutions', 'VisInstitutionsController@index');
 
 
 
