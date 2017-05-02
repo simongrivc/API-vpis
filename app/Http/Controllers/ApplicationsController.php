a644e9ce2617e236595c0c87952fbf416af5c4c4
simongrivc <simongrivc@gmail.com>
2017-05-02 16:15:27 +0200

    model update
    model update

--- app/Http/Controllers/ApplicationsController.php
+++ app/Http/Controllers/ApplicationsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
-use App\Application;
+use App\Application_view;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
