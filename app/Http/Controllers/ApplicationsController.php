4df6aa3ccf39040787051ab1320d8db0ba4f3e29
simongrivc <simongrivc@gmail.com>
2017-05-02 15:41:24 +0200

    applications auth
    applications auth

--- app/Http/Controllers/ApplicationsController.php
+++ app/Http/Controllers/ApplicationsController.php
@@ -1,7 +1,8 @@
 <?php
  
 namespace App\Http\Controllers;
- 
+
+use App\User;
 use App\Application;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -12,11 +13,22 @@ class ApplicationsController extends Controller{
 
     public function getApplications(){
  		$user = Auth::user();
- 		return $user;
+ 		if($user->fk_id_vis_institution!=null && $user->fk_user_role==2)
+ 		{
+ 			//Vrni vse prijavnice služba vpis
+ 			$application = Application::all();
+        	return response()->json($application);
+ 		}
+ 		else if($user->fk_id_vis_institution!=null && $user->fk_user_role==3)
+ 		{
+ 			//prefiltriraj prijavnice glede na id fakultete referenta
+ 			$idFakulteta = $user->fk_id_vis_institution
+ 			$applicationsWish1 = Application::::whereRaw('study_programs_calls_wish1_id != null AND program_carrier_wish1_id ==' + $idFakulteta)->get();
 
- 		//return "blabla";
-        //$application = Application::all();
-        //return response()->json($application);
+        	return response()->json($applicationsWish1);
+ 		}
+ 		else
+ 			return false;
     }
     
 
