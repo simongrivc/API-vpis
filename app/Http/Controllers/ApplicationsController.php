e422ac2b69acec8232620d8f5c007d2f5278acac
simongrivc <simongrivc@gmail.com>
2017-05-02 16:28:55 +0200

    applications sluzba vpis
    applications sluzba vpis

--- app/Http/Controllers/ApplicationsController.php
+++ app/Http/Controllers/ApplicationsController.php
@@ -16,8 +16,70 @@ class ApplicationsController extends Controller{
  		if($user->fk_user_role==2)
  		{
  			//Vrni vse prijavnice služba vpis
- 			$application = Application_view::all();
-        	return response()->json($application);
+ 			//$application = Application_view::all();
+ 			$applicationsWish1 = Application_view::where('study_programs_calls_wish1_id', '!=', null)->get();
+ 			$applicationsWish2 = Application_view::where('study_programs_calls_wish2_id', '!=', null)->get();
+ 			$applicationsWish3 = Application_view::where('study_programs_calls_wish3_id', '!=', null)->get();
+        	
+        	foreach ($applicationsWish1 as $application) {
+	        		$application->study_programs_calls_wish2_id=null;
+					$application->study_programs_wish2_is_active=null;			
+					$application->study_programs_wish2_id=null;
+					$application->study_programs_wish2_program_name=null;
+					$application->study_programs_calls_wish2_double_id=null;	
+					$application->study_programs_wish2_double_is_active=null;			
+					$application->study_programs_wish2_double_id=null;
+					$application->study_programs_wish2_double_program_name=null;		
+					$application->study_programs_calls_wish3_id=null;	
+					$application->study_programs_wish3_is_active=null;			
+					$application->study_programs_wish3_id=null;
+					$application->study_programs_wish3_program_name=null;	
+					$application->study_programs_calls_wish3_double_id=null;		
+					$application->study_programs_wish3_double_is_active=null;	
+					$application->study_programs_wish3_double_id=null;
+					$application->study_programs_wish3_double_program_name=null;
+        	}
+
+        	foreach ($applicationsWish2 as $application) {
+	        		$application->study_programs_calls_wish1_id=null;
+					$application->study_programs_wish1_is_active=null;			
+					$application->study_programs_wish1_id=null;
+					$application->study_programs_wish1_program_name=null;
+					$application->study_programs_calls_wish1_double_id=null;	
+					$application->study_programs_wish1_double_is_active=null;			
+					$application->study_programs_wish1_double_id=null;
+					$application->study_programs_wish1_double_program_name=null;		
+					$application->study_programs_calls_wish3_id=null;	
+					$application->study_programs_wish3_is_active=null;			
+					$application->study_programs_wish3_id=null;
+					$application->study_programs_wish3_program_name=null;	
+					$application->study_programs_calls_wish3_double_id=null;		
+					$application->study_programs_wish3_double_is_active=null;	
+					$application->study_programs_wish3_double_id=null;
+					$application->study_programs_wish3_double_program_name=null;
+        	}
+
+        	foreach ($applicationsWish3 as $application) {
+	        		$application->study_programs_calls_wish1_id=null;
+					$application->study_programs_wish1_is_active=null;			
+					$application->study_programs_wish1_id=null;
+					$application->study_programs_wish1_program_name=null;
+					$application->study_programs_calls_wish1_double_id=null;	
+					$application->study_programs_wish1_double_is_active=null;			
+					$application->study_programs_wish1_double_id=null;
+					$application->study_programs_wish1_double_program_name=null;		
+					$application->study_programs_calls_wish2_id=null;	
+					$application->study_programs_wish2_is_active=null;			
+					$application->study_programs_wish2_id=null;
+					$application->study_programs_wish2_program_name=null;	
+					$application->study_programs_calls_wish2_double_id=null;		
+					$application->study_programs_wish2_double_is_active=null;	
+					$application->study_programs_wish2_double_id=null;
+					$application->study_programs_wish2_double_program_name=null;
+        	}
+
+
+        	return response()->json(array_merge($applicationsWish1, $applicationsWish2,  $applicationsWish3));
  		}
  		else if($user->fk_id_vis_institution!=null && $user->fk_user_role==3)
  		{
