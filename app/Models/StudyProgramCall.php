ec9e9e325c77b2ea98115c7694a6b437782d3863
simongrivc <simongrivc@gmail.com>
2017-05-05 12:45:27 +0200

    test
    test

--- app/Models/StudyProgramCall.php
+++ app/Models/StudyProgramCall.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 class StudyProgramCall extends Model
 { 
 	//id fk_id_type fk_id_program_carrier	sequence_number	serial_id	program_name
- 	protected $fillable = ['id', 'fk_id_call_type', 'fk_id_call_group','nr_slo_eu', 'nr_without_citizenship_foreigners', 'fk_id_study_program', 'min_nr_points', 'is_active'];
+ 	protected $fillable = ['fk_id_call_type', 'fk_id_call_group','nr_slo_eu', 'nr_without_citizenship_foreigners', 'fk_id_study_program', 'min_nr_points', 'is_active'];
 
  	protected $attributes  = ['id', 'fk_id_call_type', 'fk_id_call_group', 'nr_slo_eu', 'nr_without_citizenship_foreigners', 'fk_id_study_program', 'min_nr_points', 'is_active'];
  
