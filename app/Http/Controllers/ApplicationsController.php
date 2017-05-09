24a84d0ee4fdb4de58965ecbd91339441d1deaf6
simongrivc <simongrivc@gmail.com>
2017-05-10 00:16:29 +0200

    test
    test

--- app/Http/Controllers/ApplicationsController.php
+++ app/Http/Controllers/ApplicationsController.php
@@ -277,7 +277,7 @@ class ApplicationsController extends Controller{
         //dodajanje
         if($fk_id_user && $address && $fk_id_city_address && $address_temp && $fk_id_city_address_temp && $fk_id_citizenship && $fk_id_wish1 && $fk_id_wish2 && $fk_id_wish3 && $emso && $wish1_isDouble_major &&  $wish2_isDouble_major &&  $wish3_isDouble_major && $wish1_second_wish && $wish2_second_wish && $wish3_second_wish && $timestamp_created && $timestamp_sent && $fk_id_middle_school && $contact_phone && $fk_id_klasius_srv && $fk_gained_profession && $fk_id_status && $fk_id_middle_school_completion_type)
         {
-          if($id==null)
+          if($id=='null')
           {
               $application=Application::create($request->all());
               return response()->json($application);
