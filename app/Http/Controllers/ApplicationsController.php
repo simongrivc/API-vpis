a2bc6aa669e1c12898f208100f2abd7cca9cf4a2
simongrivc <simongrivc@gmail.com>
2017-05-10 00:31:33 +0200

    null fix
    null fix

--- app/Http/Controllers/ApplicationsController.php
+++ app/Http/Controllers/ApplicationsController.php
@@ -275,7 +275,7 @@ class ApplicationsController extends Controller{
         $fk_id_middle_school_completion_type=$request->input('fk_id_middle_school_completion_type');
        
         //dodajanje
-        if($fk_id_user && $address && $fk_id_city_address && $address_temp && $fk_id_city_address_temp && $fk_id_citizenship && $fk_id_wish1 && $fk_id_wish2 && $fk_id_wish3 && $emso && $wish1_second_wish && $wish2_second_wish && $wish3_second_wish && $timestamp_created && $timestamp_sent && $fk_id_middle_school && $contact_phone && $fk_id_klasius_srv && $fk_gained_profession && $fk_id_status && $fk_id_middle_school_completion_type)
+        if($fk_id_user && $address && $fk_id_city_address && $address_temp && $fk_id_city_address_temp && $fk_id_citizenship && $fk_id_wish1 && $fk_id_wish2 && $fk_id_wish3 && $emso && $timestamp_created && $timestamp_sent && $fk_id_middle_school && $contact_phone && $fk_id_klasius_srv && $fk_gained_profession && $fk_id_status && $fk_id_middle_school_completion_type)
         {
           if($id==null)
           {
