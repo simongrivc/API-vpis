64bdb654d65745e8847da266f9d274966dbfade0
TursicKlemen <tursic.klemen@gmail.com>
2017-05-28 09:31:02 +0200

    Cont, upload

---
+++ app/Http/Controllers/RicDataUploadController.php
@@ -0,0 +1,61 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use App\StudyProgram;
+use App\StudyProgramCallView;
+use App\StudyProgramCall;
+use App\Application_view;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+ 
+class RicDataUploadController extends Controller{
+
+    public function index(){
+ 
+        $studyPrograms = StudyProgram::all();
+        return response()->json($studyPrograms);
+    }
+	
+	public function uploadFile(Request $request){
+		if ($request->hasFile('maturant')) {
+			if ($request->file('maturant')->isValid()) {
+				
+				$txt_file    = file_get_contents($request->file('maturant'));
+				$rows        = explode("\n", $txt_file);
+				array_shift($rows);
+				
+				foreach($rows as $row => $data)
+				{
+					$row_data = explode('Q', $data);
+				
+					$info[$row]['emso']           = $row_data[0];
+					$info[$row]['ime']         = $row_data[1];
+					$info[$row]['priimek']  = $row_data[2];
+					$info[$row]['uspeh']       = $row_data[3];
+				
+					echo 'Row ' . $row . ' emso: ' . $info[$row]['emso'] . '<br />';
+					/*
+					echo 'Row ' . $row . ' NAME: ' . $info[$row]['name'] . '<br />';
+					echo 'Row ' . $row . ' DESCRIPTION: ' . $info[$row]['description'] . '<br />';
+					echo 'Row ' . $row . ' IMAGES:<br />';*/
+				
+					//display images
+					/*$row_images = explode(',', $info[$row]['images']);
+				
+					foreach($row_images as $row_image)
+					{
+						echo ' - ' . $row_image . '<br />';
+					}
+					*/
+					echo '<br />';
+				}
+				
+			}
+		}
+	} 
+
+}
+?>
