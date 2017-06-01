f3ce0e1122509f203ecaa7c4bc4ab3095e2b7606
TursicKlemen <tursic.klemen@gmail.com>
2017-06-01 15:29:03 +0200

    Testing

--- app/Http/Controllers/RicDataUploadController.php
+++ app/Http/Controllers/RicDataUploadController.php
@@ -23,7 +23,10 @@ class RicDataUploadController extends Controller{
 		
 		if ($request->hasFile('maturant')) {
 			if ($request->file('maturant')->isValid()) {
-				echo "berem dat";
+				//echo "berem dat";
+				var_dump($request->file('maturant')->getClientOriginalExtension());
+				var_dump($request->file('maturant')->getMimeType());
+				die();
 				$txt_file    = file_get_contents($request->file('maturant'));
 				$rows        = explode("\n", $txt_file);
 				array_shift($rows);
@@ -31,17 +34,38 @@ class RicDataUploadController extends Controller{
 				foreach($rows as $row => $data)
 				{
 					$row_data = explode('Q', $data);
-				
-					$info[$row]['emso']           = $row_data[0];
-					$info[$row]['ime']         = $row_data[1];
-					$info[$row]['priimek']  = $row_data[2];
-					$info[$row]['uspeh']       = $row_data[3];
-					$info[$row]['opravil']       = $row_data[4];
-					$info[$row]['uspeh3l']       = $row_data[5];
-					$info[$row]['uspeh4l']       = $row_data[6];
-					$info[$row]['tip']       = $row_data[7];
-					$info[$row]['srSola']       = $row_data[8];
-					$info[$row]['poklic']       = $row_data[9];
+					
+					$emso = $row_data[0];
+					$ime = $row_data[1];
+					$priimek = $row_data[2];
+					$uspeh = $row_data[3];
+					$opravil = $row_data[4];
+					$uspeh3l = $row_data[5];
+					$uspeh4l = $row_data[6];
+					$tip = $row_data[7];
+					$srSola = $row_data[8];
+					$poklic = $row_data[9];
+					
+					$info[$row]['emso']      = $emso;
+					$info[$row]['ime']       = $ime;
+					$info[$row]['priimek']   = $priimek;
+					$info[$row]['uspeh']     = $uspeh;
+					$info[$row]['opravil']   = $opravil;
+					$info[$row]['uspeh3l']   = $uspeh3l;
+					$info[$row]['uspeh4l']   = $uspeh4l;
+					$info[$row]['tip']       = $tip;
+					$info[$row]['srSola']    = $srSola;
+					$info[$row]['poklic']    = $poklic;
+					
+					$user = DB::table('applications')->where('emso', $info[$row]['emso'])->first();
+					if($user){
+						//obstaja prijava za tale emso, vpišemo zaključno oceno in poklic
+						$id = DB::table('ric_candidates')->insertGetId(
+                            ['emso' => $emso, 'fk_profession' => $poklic, 'points_grade' => $uspeh, 'success' => $opravil, 'grade3' => $uspeh3l, 'grade4' => $uspeh4l,
+							 'fk_type' => $tip, 'fk_middle_school' => $srSola]
+                        );						
+						
+					}
 					
 				
 					/*echo 'Row ' . $row . ' emso: ' . $info[$row]['emso'] . '<br />';
@@ -60,7 +84,7 @@ class RicDataUploadController extends Controller{
 					*/
 					//echo '<br />';
 				}
-				var_dump($info);				
+				//var_dump($info);				
 				
 			}
 			else{
