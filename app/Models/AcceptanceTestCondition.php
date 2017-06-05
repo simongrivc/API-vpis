014e6d18a61fa0dfedcdb6bc7f0cb7c19ad6d7ac
simongrivc <simongrivc@gmail.com>
2017-06-05 11:58:17 +0200

    test2

--- app/Models/AcceptanceTestCondition.php
+++ app/Models/AcceptanceTestCondition.php
@@ -7,8 +7,8 @@ class AcceptanceTestCondition extends Model
 	
  	protected $fillable = ['id', 'min_points','max_points', 'fk_id_program_call_conditions'];
 
- 	protected $attributes  = ['id', 'min_points','max_points', 'fk_id_program_call_conditions'];
-
+ /*	protected $attributes  = ['id', 'min_points','max_points', 'fk_id_program_call_conditions'];
+*/
  
  	protected $table = 'acceptance_test_condition';
  	//da eloquent ne dodaja timestamp v bazo
