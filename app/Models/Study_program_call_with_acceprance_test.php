7709d7c170c8cfb0c6c89ec8234fbfcc1eb8bbb2
simongrivc <simongrivc@gmail.com>
2017-06-06 00:03:45 +0200

    typo fix
    typo fix

--- app/Models/Study_program_call_with_acceprance_test.php
+++ app/Models/Study_program_call_with_acceprance_test.php
@@ -10,7 +10,7 @@ class Study_program_call_with_acceprance_test extends Model
  	protected $attributes  = ['fk_program_call_id', 'program_name', 'fk_id_program_carrier', 'fk_id_call_type'];
 
  
- 	protected $table = 'study_program_calls_with_acceprance_tests';
+ 	protected $table = 'study_program_calls_with_acceptance_tests';
  	//da eloquent ne dodaja timestamp v bazo
  	public $timestamps  = false;
 }
