c78ae392e2e5e7b4facee6a620af82d1085d0538
simongrivc <simongrivc@gmail.com>
2017-06-05 11:22:05 +0200

    view
    view

--- app/Models/AcceptanceTestConditionView.php
+++ app/Models/AcceptanceTestConditionView.php
@@ -10,7 +10,7 @@ class AcceptanceTestConditionView extends Model
  	protected $attributes  = ['id', 'condition_weight', 'fk_condition_code_id', 'fk_condition_group', 'id_acceptance_test_condition','min_points','max_points','condition_code_name','fk_program_call_id','fk_id_study_program','program_name','fk_id_program_carrier'];
 
  
- 	protected $table = 'acceptance_test_conditions';
+ 	protected $table = 'acceptance_test_conditions_view';
  	//da eloquent ne dodaja timestamp v bazo
  	public $timestamps  = false;
 }
